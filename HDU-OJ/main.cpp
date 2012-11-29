ccc0894bb91b3b2e6bfdee109f395a226c2cb4f2
VincenKoo <vincenkoo@gmail.com>
2012-11-29 21:57:58 +0800

    Initial Commit

---
+++ HDU-OJ/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HDU-OJ
+//
+//  Created by Vincen on 12-11-29.
+//  Copyright (c) 2012年 Vincen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
